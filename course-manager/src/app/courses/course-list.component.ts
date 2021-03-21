662921c6993975db1da3b66b30cc1c3334c3dc05
João Pablo <jp.pablolima@gmail.com>
2021-03-21 12:28:09 -0300

    feat: List Courses

---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+import {Course} from './course';
+import {CourseService} from './course.service'
+
+
+@Component({
+    
+    templateUrl: './course-list.component.html' 
+})
+export class CourseListComponent implements OnInit{
+    
+    filteredCourses: Course[] = [];
+    _courses: Course[] = [];
+    _filterBy: string;
+
+
+    constructor(private courseService: CourseService){ }
+
+    ngOnInit(): void{
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string){
+        this._filterBy = value
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLowerCase().indexOf(this._filterBy.toLowerCase()) > -1);
+    }
+
+}
---
+++ course-manager/src/app/courses/course.ts
@@ -0,0 +1,13 @@
+import { StringMap } from "@angular/compiler/src/compiler_facade_interface";
+
+export class Course{
+    id: number;
+    name: string;
+    imageUrl: string;
+    price: number;
+    code: String;
+    duration: number;
+    rating: number;
+    releaseDate: string
+    description: string
+}
